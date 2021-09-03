fd807d0614b67eed1ce2a5052e1d1176ce1042c3
theripper93 <tsplab@gmail.com>
2021-09-03 17:35:20 +0200

    a

--- scripts/config.js
+++ scripts/config.js
@@ -16,5 +16,5 @@ Hooks.once('ready', async function() {
 
 function tokenZRefresh(wrapped,...args) {
     wrapped(...args);
-    this.zIndex = this.data.elevation || 0;
+    this.zIndex = 2 + this.data.elevation*10 -this.data.width - this.data.height + this._controlled + this.data.flags["token-z"]?.zIndex || 0;
 }
