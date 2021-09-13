6d62244e7d20ab6ea1c741623bd949da179246d7
theripper93 <tsplab@gmail.com>
2021-09-13 17:09:30 +0200

    a

--- scripts/main.js
+++ scripts/main.js
@@ -9,17 +9,11 @@ Hooks.once("init", async function () {
     const controlled = this._controlled ? 1 : 0;
     this.zIndex = 2 + this.data.elevation * 10 - this.data.width - this.data.height + controlled + flag;
   }
-
-  libWrapper.register(
-    "token-z",
-    "Token.prototype.refresh",
-    tokenZRefresh,
-    "WRAPPER"
-  );
+  libWrapper.register("token-z","Token.prototype.refresh",tokenZRefresh,"WRAPPER");
 });
 
 Hooks.on("renderTokenConfig", (app, html, data) => {
-  let zIndex = app.object.getFlag("token-z", "zIndex") || 0;
+  let zIndex = app.token.getFlag("token-z", "zIndex") || 0;
 
   let newHtml = `
   <div class="form-group">
