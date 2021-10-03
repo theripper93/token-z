d06952c19dfbd812c2c5f6c9896f46a6d05042c4
theripper93 <tsplab@gmail.com>
2021-10-03 20:53:45 +0200

    a

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,8 @@ Hooks.once("init", async function () {
     wrapped(...args);
     const flag = this.data.flags["token-z"]?.zIndex ?? 0;
     const controlled = this._controlled ? 1 : 0;
-    this.zIndex = 2 + this.data.elevation * 10 - this.data.width - this.data.height + controlled + flag;
+    const defeated = this.actor?.effects?.find(e => e.getFlag("core", "statusId") === CONFIG.Combat.defeatedStatusId) ? -1000 : 0;
+    this.zIndex = 2 + this.data.elevation * 10 - this.data.width - this.data.height + controlled + flag + defeated;
   }
   libWrapper.register("token-z","Token.prototype.refresh",tokenZRefresh,"WRAPPER");
 });
@@ -23,3 +24,4 @@ Hooks.on("renderTokenConfig", (app, html, data) => {
   `;
   html.find('input[name="elevation"]').closest(".form-group").after(newHtml);
 });
+
