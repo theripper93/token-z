a80400f7776ebc4276729855e4e3fe7e06f41c00
theripper93 <tsplab@gmail.com>
2025-05-09 00:23:03 +0200

    a

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ Object.defineProperty(TokenDocument.prototype, "sort" , {
     const zIndexOverride = this["token-z"]?.zIndexOverride;
     if(typeof zIndexOverride === "number") return zIndexOverride;
 
-    const flag = this.flags["token-z"]?.zIndex ?? 0;
+    const flag = this._source.sort ?? 0;
     const controlled = this._object?.controlled ? 1 : 0;
     const defeated = this.actor?.statuses?.has(CONFIG.specialStatusEffects.DEFEATED) ? -1000 : 0;
     return 2 - this.width - this.height + controlled + flag + defeated;
@@ -14,27 +14,12 @@ Object.defineProperty(TokenDocument.prototype, "sort" , {
 })
 
 Hooks.on("renderTokenHUD", (app, html, data) => {
-  html = html[0] ?? html;
-  html.querySelector(`[data-action="sort-up"]`).remove();
-  html.querySelector(`[data-action="sort-down"]`).remove();
+  html.querySelectorAll(`[data-action="sort"]`).forEach(el => el.remove());
 })
 
 Hooks.on("controlToken", (token, controlled) => {
   if(controlled) token.mesh.zIndex += 1;
 })
-
-Hooks.on("renderTokenConfig", (app, html, data) => {
-  let zIndex = app.token.getFlag("token-z", "zIndex") || 0;
-
-  let newHtml = `
-  <div class="form-group">
-              <label>${game.i18n.localize("tokenz.tokenconfig.zindex")}</label>
-              <input type="number" name="flags.token-z.zIndex" placeholder="units" value="${zIndex}">
-          </div>
-  `;
-  html.find('input[name="rotation"]').closest(".form-group").after(newHtml);
-  app.setPosition({height: "auto"});
-});
 
 Hooks.on("refreshToken", (token) => {
   canvas.tokens.objects.sortDirty = canvas.primary.sortDirty = true;
